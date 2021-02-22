9ffca13bb4de628c6385f07c80fc558ae09c5da3
Alexandre Piveteau <alexandre@piveteau.email>
2021-02-22 15:08:55 +0100

    Add a basic project structure and CI

---
+++ src/main/kotlin/markdown/party/main.kt
@@ -0,0 +1,13 @@
+package markdown.party
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+
+/**
+ * The main entry point of the application.
+ */
+fun main() = runBlocking {
+    println("Hello world.")
+    delay(2000)
+    println("Lorem ipsum.")
+}
---
+++ src/test/kotlin/markdown/party/BootstrapTest.kt
@@ -0,0 +1,15 @@
+package markdown.party
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class BootstrapTest {
+
+    /**
+     * This test always succeeds.
+     */
+    @Test
+    fun `testing is properly set up`() {
+        assertEquals(4, 2 + 2)
+    }
+}
