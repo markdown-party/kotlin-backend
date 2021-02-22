ae1a652d3aa85ee502d50d081677a1b4ce6da842
Alexandre Piveteau <alexandre@piveteau.email>
2021-02-22 16:47:10 +0100

    Slightly improve main

--- src/main/kotlin/markdown/party/main.kt
+++ src/main/kotlin/markdown/party/main.kt
@@ -9,11 +9,16 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
 /**
+ * Returns the port number to use when running the web application. Defaults to 1234 if no port is
+ * specified.
+ */
+private val Port = System.getProperty("PORT")?.toIntOrNull() ?: 1234
+
+/**
  * The main entry point of the application.
  */
 fun main() {
-    val port = System.getenv("PORT")?.toIntOrNull() ?: 1234
-    val server = embeddedServer(Netty, port = port) {
+    val server = embeddedServer(Netty, port = Port) {
         routing {
             get { call.respondText("Hello world.") }
         }
